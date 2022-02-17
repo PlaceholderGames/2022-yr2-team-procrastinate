4b7748a2fa1e65dbba815353c32151878f5f0242
LiamFeasey <30005249@students.southwales.ac.uk>
2022-02-17 22:49:03 +0000

    Added some basic textures for the map using tilemaps, re-did some of the movement code so that it uses rigidbodies instead of transform to move, made the basic layout of the map

--- Assets/Scripts/BasicMovment.cs
+++ Assets/Scripts/BasicMovment.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class BasicMovment : MonoBehaviour
 {
-
-    public Animator animator;
-
-    public Animator animator;
+    [SerializeField] bool collidingWithStaticObject = false;
+    [SerializeField] private Rigidbody2D playerRigidBody = null;
+    [SerializeField] float movementSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = this.GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"), 0.0f);
-
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Magnitute", movement.magnitude);
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (collidingWithStaticObject == false)
+        {
+            //Moving using Rigidbody2D.MovePosition so that propper collision detection works. 
+            //old method of transform.position doesn't work well with collision detection
+            playerRigidBody.MovePosition(playerRigidBody.position + (movementSpeed * movement) * Time.deltaTime);
+        }
+        
+        //flips the characters sprite so they face the direction they're walking
+        if (movement.x < 0)
+        {
+            GameObject.Find("Jeremy").GetComponent<SpriteRenderer>().flipX = true;
+            GameObject.Find("Jeremy").GetComponent<BoxCollider2D>().offset = new Vector2(0.039f, -0.1402018f);
+        }
+        else if (movement.x > 0)
+        {
+            GameObject.Find("Jeremy").GetComponent<SpriteRenderer>().flipX = false;
+            GameObject.Find("Jeremy").GetComponent<BoxCollider2D>().offset = new Vector2(-0.02473149f, -0.1402018f);
+        }
     }
+    
+    //private void OnTriggerEnter2D(Collider2D collider)
+    //{
+    //    print("Colliding with object!");
+    //    if (collider.transform.parent.GetComponent<Rigidbody2D>().bodyType == RigidbodyType2D.Static)
+    //    {
+    //        print("Colliding with wall!");
+    //        collidingWithStaticObject = true;
+    //    }
+    //}
 
     //private void OnTriggerExit2D(Collider2D collider)
     //{
