6f267fd9a57e6b1e5993b2dbda8482878305d552
LiamFeasey <30005249@students.southwales.ac.uk>
2022-02-23 20:06:07 +0000

    fixing BasicMovement Script

--- Assets/Scripts/BasicMovment.cs
+++ Assets/Scripts/BasicMovment.cs
@@ -7,11 +7,21 @@ public class BasicMovment : MonoBehaviour
 
     public Animator animator;
 
+    [SerializeField] bool collidingWithStaticObject = false;
+    [SerializeField] private Rigidbody2D playerRigidBody = null;
+    [SerializeField] float movementSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"), 0.0f);
+        Vector2 movement = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        playerRigidBody.MovePosition(playerRigidBody.position + (movementSpeed * movement) * Time.deltaTime);
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
